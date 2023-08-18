9870c3068162734a082b0cf784313cd7dcc9e344
Michael Carroll <mcarroll138@gmail.com>
2023-08-18 11:02:59 -0700

    add put route and test.

--- LocalBusinessApi/Controllers/BusinessesController.cs
+++ LocalBusinessApi/Controllers/BusinessesController.cs
@@ -40,7 +40,42 @@ namespace LocalBusinessApi.Controllers
         {
             _db.Businesses.Add(business);
             await _db.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId } business);
+            return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+        }
+
+        //Put: api/businesses/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Business business)
+        {
+            if (id != business.BusinessId)
+            {
+                return BadRequest();
+            }
+
+            _db.Businesses.Update(business);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusinessExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool BusinessExists(int id)
+        {
+            return _db.Businesses.Any(e => e.BusinessId == id);
         }
     }
 }
